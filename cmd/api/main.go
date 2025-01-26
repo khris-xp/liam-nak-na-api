a5dc45b46b4ad026afa3aba5ac65b5c4dcc27198
khris-xp <khrisbharmmano@gmail.com>
2025-01-26 18:54:54 +0700

    feat: allow core

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"liam-nak-na-api/internal/ports/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -12,6 +13,13 @@ func main() {
 	triangleHandler := http.NewTriangleHandler(triangleService)
 
 	e := echo.New()
+
+	// Add CORS middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"*"},
+	}))
 
 	e.POST("/triangle", triangleHandler.ClassifyTriangle)
 
