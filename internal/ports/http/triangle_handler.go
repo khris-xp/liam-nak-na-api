fe630577a36ecbcb47cd5617d9ed8ce94e176760
TeetouchQQ <teawkrub.ghs@gmail.com>
2025-03-02 15:42:16 +0700

    Update triangle_handler.go

--- internal/ports/http/triangle_handler.go
+++ internal/ports/http/triangle_handler.go
@@ -84,6 +84,11 @@ func validateAndParseFloat(input string) (float64, error) {
 		return 0, errors.New("Please fill out this field.")
 	}
 
+	parts := strings.Split(input, ".")
+	if len(parts) > 1 && len(parts[1]) > 3 {
+		return 0, errors.New("Invalid inputs : Enter a number between 0-999999.")
+	}
+	
 	num, err := strconv.ParseFloat(input, 64)
 	if err != nil {
 		return 0, errors.New("Invalid inputs : Enter a number between 0-999999.")
